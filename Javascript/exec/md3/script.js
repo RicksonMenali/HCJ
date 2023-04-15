888f52bc88defe1edc43da310ca134121fabdfd1
RicksonMenali <127509240+RicksonMenali@users.noreply.github.com>
2023-04-15 12:25:45 -0300

    ok

---
+++ Javascript/exec/md3/script.js
@@ -0,0 +1,23 @@
+var p = document.getElementsByTagName('p');
+
+//alert(p[1].innerHTML);
+var elementos = document.getElementsByClassName('box')
+
+var el = elementos[0];
+
+el.style.width = 100;
+el.style.height = 100;
+el.style.backgroundColor = 'red';
+
+
+
+
+
+
+/*for (var i = 0; i < elementos.length; i++){
+    var el = elementos[i];
+
+    el.style.width = 100;
+    el.style.height = 100;
+    el.style.backgroundColor = 'red';
+}
